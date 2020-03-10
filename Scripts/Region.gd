c1ff4c41fa71b9b4b917f20d41a652ae6f2d94b0
Aryeh <aryehzinn@hotmail.com>
2020-03-11 07:25:26 +1100

    Added an outline that activates when the mouse hovers over a region. Need to add more regions now

--- Scripts/Region.gd
+++ Scripts/Region.gd
@@ -3,12 +3,11 @@ extends Node2D
 export (Texture) var region_details
 export (Texture) var region_outline
 export (Texture) var region_overlay
-export (bool) var editor_update
+export (bool) var update
 export (Color) var occupied_color
 export (float) var change_duration
-var outline_color = Color(0,0,0,1)
+onready var outline_color = Color(0,0,0,1)
 var elapsed = 0.0
-var has_changed = false
 
 func _ready():
 	change_region_sprite()
@@ -16,22 +15,17 @@ func _ready():
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.pressed:
-			has_changed = true
+			update = true
 			elapsed = 0.0
 			if Input.is_action_pressed("lmb"):
 				occupied_color = Color(1, 0, 0, 0.4)
-				outline_color = Color(1, 0, 0, 1)
 			elif Input.is_action_pressed("rmb"):
 				occupied_color = Color(0, 0, 0, 0)
-				outline_color = Color(0, 0, 0, 1)
 
 func _process(delta):
-	if editor_update:
+	if update:
 		if Engine.editor_hint:
 			change_region_sprite()
-			set_occupied(occupied_color, Color(occupied_color.r,occupied_color.g,occupied_color.b, 1), delta)
-
-	if has_changed:
 		set_occupied(occupied_color, outline_color, delta)
 
 func change_region_sprite():
@@ -50,12 +44,12 @@ func transition_color(delta, ui, dest_color):
 	elif elapsed >= change_duration:
 		ui.modulate = dest_color
 		elapsed = 0.0
-		has_changed = false
+		update = false
 
 func change_outline(color):
 	outline_color = color
 	elapsed = 0.0
-	has_changed = true
+	update = true
 
 func _on_Area2D_mouse_entered():
 	change_outline(Color(1, 1, 0, 1))
