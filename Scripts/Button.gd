2e3a6a89ae0c909d332f849166575166d0a537c3
Aryeh <aryehzinn@hotmail.com>
2020-03-18 21:12:03 +1100

    Cleaned up my code

--- Scripts/Button.gd
+++ Scripts/Button.gd
@@ -14,21 +14,21 @@ func _input_event(viewport, event, shape_idx):
 		if event.pressed:
 			if Input.is_action_just_pressed("lmb"):
 				retracting = true
-				$ButtonOutline.modulate = fill_click
-				$ButtonBorder.modulate = border_click
+				set_color(fill_click, border_click)
 				get_parent().get_parent().deactivate_panel()
 
 func on_mouse_exit():
 	if not retracting:
-		$ButtonOutline.modulate = fill_idle
-		$ButtonBorder.modulate = border_idle
+		set_color(fill_idle, border_idle)
 
 func reset():
 	retracting = false
-	$ButtonOutline.modulate = fill_idle
-	$ButtonBorder.modulate = border_idle
+	set_color(fill_idle, border_idle)
 
 func on_hover():
 	if not retracting:
-		$ButtonOutline.modulate = fill_hover
-		$ButtonBorder.modulate = border_hover
+		set_color(fill_hover, border_hover)
+
+func set_color(fill, border):
+	$ButtonOutline.modulate = fill
+	$ButtonBorder.modulate = border
