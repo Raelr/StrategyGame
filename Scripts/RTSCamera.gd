c97e10537f1bd489dafc55ba217f227d128446e5
Aryeh <aryehzinn@hotmail.com>
2020-03-21 22:23:57 +1100

    Fixed camera bugs. Should be moving fine now. Need to fix some code issues

--- Scripts/RTSCamera.gd
+++ Scripts/RTSCamera.gd
@@ -1,19 +1,17 @@
 extends Camera2D
 
-onready var screen = get_viewport().size
 export (Vector2) var mouse_offset
+export (Vector2) var friction
 export (float) var move_speed
+export (float) var max_distance
 
-var above_x = false
-var below_x = false
-var above_y = false
-var below_y = false
+const VELOCITY_DELTA = 0.01
+const MAX_ACCELERATION = 100
 
-func out_of_bounds():
-	var mouse = get_viewport().get_mouse_position()
-	var x = mouse.x > screen.x - mouse_offset.x or mouse.x < mouse_offset.x
-	var y = mouse.y > screen.y - mouse_offset.y or mouse.y < mouse_offset.y
-	return x or y
+onready var screen = get_viewport().size
+onready var velocity = Vector2(0,0)
+onready var max_dist = Vector2(position.x + max_distance, position.y + max_distance)
+onready var min_dist = Vector2(position.x - max_distance, position.y - max_distance)
 
 func above_threshold_x(mouse_pos):
 	return mouse_pos.x > screen.x - mouse_offset.x
@@ -28,11 +26,11 @@ func below_threshold_y(mouse_pos):
 	return mouse_pos.y < mouse_offset.y
 
 func _process(delta):
-	var velocity = adjust_velocity(Vector2(0,0))
-	position = position.linear_interpolate(velocity, delta)
+	position = position.linear_interpolate(adjust_velocity(), delta)
 
-func adjust_velocity(velocity):
+func adjust_velocity():
 	var mouse = get_viewport().get_mouse_position()
+	
 	if above_threshold_x(mouse):
 		velocity.x += move_speed
 	if below_threshold_x(mouse):
@@ -41,7 +39,24 @@ func adjust_velocity(velocity):
 		velocity.y += move_speed
 	if below_threshold_y(mouse):
 		velocity.y -= move_speed
-	var next_pos = position + velocity
-	return next_pos
+	
+	apply_friction()
+	
+	velocity = Vector2(clamp(velocity.x, -MAX_ACCELERATION, MAX_ACCELERATION), clamp(velocity.y, -MAX_ACCELERATION, MAX_ACCELERATION))
+	
+	if abs(velocity.x) < VELOCITY_DELTA:
+		velocity.x = 0
+	if abs(velocity.y) < VELOCITY_DELTA:
+		velocity.y = 0
+	
+	return Vector2(clamp((position.x + velocity.x), min_dist.x, max_dist.x), clamp((position.y + velocity.y), min_dist.y, max_dist.y))
 
-
+func apply_friction():
+	if velocity.x > 0:
+		velocity.x -= friction.x
+	elif velocity.x < 0:
+		velocity.x += friction.x
+	if velocity.y > 0:
+		velocity.y -= friction.y
+	elif velocity.y < 0:
+		velocity.y += friction.y
