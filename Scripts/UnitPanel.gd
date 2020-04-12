cdd214d329251ec4c290253da3bd38dc21ff2cd1
Aryeh <aryehzinn@hotmail.com>
2020-04-12 12:41:13 +1000

    Added basic animation for unitpanel

--- Scripts/UnitPanel.gd
+++ Scripts/UnitPanel.gd
@@ -1,10 +1,16 @@
 extends Menu
 
+var is_active = false
+
 func populate_ui(unit_name, unit_attack, unit_defence, unit_health, unit_color):
-	$UnitName.text = unit_name
-	$AttackValue.text = str(unit_attack)
-	$DefenceValue.text = str(unit_defence)
-	$HealthValue.text = str(unit_health["current"])
-	$HealthBar.max_value = unit_health["max"]
-	$HealthBar.value = unit_health["current"]
-	$UnitIcon.material.set_shader_param("unit_color", unit_color)
+	$AnchorNode/UnitName.text = unit_name
+	$AnchorNode/AttackValue.text = str(unit_attack)
+	$AnchorNode/DefenceValue.text = str(unit_defence)
+	$AnchorNode/HealthValue.text = str(unit_health["current"])
+	$AnchorNode/HealthBar.max_value = unit_health["max"]
+	$AnchorNode/HealthBar.value = unit_health["current"]
+	$AnchorNode/UnitIcon.material.set_shader_param("unit_color", unit_color)
+	if not is_active:
+		visible = true
+		$AnchorNode/AnimationPlayer.play("activated")
+
