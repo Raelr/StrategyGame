fe0a8bab2af80c3bbd84dd843c787302a57b5765
Aryeh <aryehzinn@hotmail.com>
2020-04-14 09:31:04 +1000

    re-organised main combat loop to allow the victor to be carried over multiple conditiions

--- Scripts/CommandManager.gd
+++ Scripts/CommandManager.gd
@@ -7,14 +7,14 @@ func process_command(regions):
 	for region in evaluated_regions:
 		var region_details = regions[region]
 		var moving_units = region_details["moving"]
-		# Check if there is more than one unit moving into a region. If there is,
-		# check if there is any combat that needs to occur.
-		# CASE 1: Two opposing players are moving into the same region
-		if moving_units.size() > 1:
-			# Check for the factions involved in this.
-			if region_details["factions"].size() > 1:
-				var units_by_faction = {}
-				var victor = null
+		var occupying_units = region_details["occupying"]
+		# Check if there is more than one faction involved in this region.
+		# If there is then there is most likely going to be a combat.
+		if region_details["factions"].size() > 1:
+			var victor = null
+			var units_by_faction = {}
+			# CASE 1: Two opposing players are moving into the same region
+			if moving_units.size() > 1:
 				# For each faction - get their units and combine their stats. 
 				for faction in region_details["factions"]:
 					var evaluated = Array()
@@ -27,21 +27,23 @@ func process_command(regions):
 								victor = deal_reciprocal_damage(stack, other_stack)
 								# Set the current stack as damaged already
 								evaluated.push_back(stack)
-					# Now take the damage taken and subtract it from all units in the stack
-					for faction in units_by_faction.keys():
-						# Get the current faction. 
-						var faction_units = get_units_from_faction(faction, moving_units)
-						var damage = units_by_faction[faction]["damage_taken"]
-						var idx = 0
-						# Make sure all damage is distributed amongst units
-						while damage > 0:
-							var unit = faction_units[idx]
-							damage = unit.on_damage_dealt(damage)
-							idx += 1
-				if victor:
-					# If there is a victor then move the units of the victor over.
-					var victor_units = get_units_from_faction(victor["faction"], moving_units)
-					move_units_in_group(victor_units, region)
+			elif occupying_units.size() > 0 and moving_units > 0:
+				pass
+			# Now take the damage taken and subtract it from all units in the stack
+			for faction in units_by_faction.keys():
+				# Get the current faction. 
+				var faction_units = get_units_from_faction(faction, moving_units)
+				var damage = units_by_faction[faction]["damage_taken"]
+				var idx = 0
+				# Make sure all damage is distributed amongst units
+				while damage > 0:
+					var unit = faction_units[idx]
+					damage = unit.on_damage_dealt(damage)
+					idx += 1
+			if victor:
+				# If there is a victor then move the units of the victor over.
+				var victor_units = get_units_from_faction(victor["faction"], moving_units)
+				move_units_in_group(victor_units, region)
 		else:
 			# If not combat has occurred then just move the units
 			move_units_in_group(moving_units, region)
@@ -76,11 +78,14 @@ func damage_stack(stack, damage):
 	stack["damage_taken"] += damage
 
 func deal_reciprocal_damage(stack_a, stack_b):
-	var victor = null
 	# Damage other stack
 	damage_stack(stack_b, stack_a["attack"] - stack_b["defence"])
 	# Take damage from the other stack
 	damage_stack(stack_a, stack_b["attack"] - stack_a["defence"])
+	return get_victor(stack_a, stack_b)
+
+func get_victor(stack_a, stack_b):
+	var victor = null
 	if is_defeated(stack_a) and not is_defeated(stack_b):
 		victor = stack_b
 	if is_defeated(stack_b) and not is_defeated(stack_a):
