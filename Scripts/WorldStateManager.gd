74542a872bbcdcbdcb1d2594e6344a8745ba3ffd
Aryeh <aryehzinn@hotmail.com>
2020-04-26 09:56:14 +1000

    Added functionality to check if regions are still active

--- Scripts/WorldStateManager.gd
+++ Scripts/WorldStateManager.gd
@@ -29,24 +29,40 @@ func add_region(region):
 		print("Region: " + region.region_name + " added to world_state!")
 
 func add_moving_units(current_region, destination_region, faction, unit):
-	remove_previous_moves(unit)
+	remove_previous_moves(unit, faction)
 	add_region(destination_region)
 	if not world_state[destination_region].moving.has(unit):
 		world_state[destination_region].moving[unit] = {
 			"moving_from" : current_region,
 			"speed" : 0
 		}
+		if not world_state[destination_region].factions.has(faction):
+			world_state[destination_region].factions.push_back(faction)
+
 		print("Added " + unit.name + " as moving to region: " + destination_region.region_name +  " from: " + current_region.region_name)
 		print("With speed: " + str(0))
 
-func remove_previous_moves(unit):
+func remove_previous_moves(unit, faction):
 	var neighbours = unit.get_possible_paths()
 	for neighbour in neighbours:
 		if world_state.has(neighbour):
 			if world_state[neighbour].moving.has(unit):
 				world_state[neighbour].moving.erase(unit)
 				print("\nRemoving move command for " + unit.name + " to move to: " + neighbour.region_name)
-	
+				if not faction_in_region(faction, neighbour):
+					world_state[neighbour].factions.erase(faction)
+				if no_moving_units(world_state[neighbour]):
+					if no_occupations(world_state[neighbour]):
+						remove_empty_regions(world_state[neighbour])
+
+func faction_in_region(faction, region):
+	var has_faction = false
+	for unit in world_state[region].moving.keys():
+		if unit.faction == faction:
+			has_faction = true
+			print("Faction: " + str(faction) + " still exists in region!")
+	return has_faction
+
 func remove_empty_regions(region):
 	if world_state.has(region):
 		if no_factions(world_state[region]):
