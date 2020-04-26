0e5bbe0f27757e408157ae0a68db2a4325410640
Aryeh <aryehzinn@hotmail.com>
2020-04-27 07:55:04 +1000

    Added comments to lay out plan for upcoming week

--- Scripts/WorldStateManager.gd
+++ Scripts/WorldStateManager.gd
@@ -3,19 +3,29 @@ extends Node2D
 var world_state = {}
 var commands := Array()
 
-# Outline of world_state storage variable.
-# var region_state = {
-#	 "factions" : Array(),
-#	 "occupying" : Array(),
-#	 "moving": Array()
-# }
+# This will be the entry which deals with unit movement and prioritisation. 
 
-# Outline of movement variable
-# var moving_unit = {
-# 	 "moving_from" : null,
-# 	 "moving_to" : null,
+# var move_command = {
+#	 "unit" : null,
+#	 "destination" : null,
 # 	 "speed" : 0
 # }
+
+# When registering a unit, add their details to the region_state table
+# This should provide details about the region's occupation details
+# Key should be the region's name (Weslund, Ososia...etc)
+
+# var region_state = {
+# 	region: NodePath (region)
+# 	occupier: NodePath (unit)
+#	occupying_faction: faction (enum)
+#	bonus : 0
+# }
+
+# TODO: Change setup to work with new plan.
+# TODO: Add region_state for every region (or at least those with units in them) to world_state.
+# TODO: Add move commands when right clicking units. 
+# TODO: Add ability to remove element from heap (for when a command is not fully set)
 
 func add_region(region):
 	if not world_state.has(region):
@@ -163,6 +173,9 @@ func remove_first():
 	sort_down(commands.front())
 	return first_item
 
+func find(unit): 
+	pass
+
 func sort_down(command, start_idx = 0):
 	var size : int = commands.size()
 	var child_idx_left: int = (start_idx * 2) + 1
@@ -177,6 +190,10 @@ func sort_down(command, start_idx = 0):
 		if command.speed < commands[swap_idx]:
 			swap(command, start_idx, commands[swap_idx], swap_idx)
 			start_idx = swap_idx
+		else:
+			return
+	else:
+		return
 
 func swap(command_a, command_a_idx, command_b, command_b_idx):
 	commands[command_a_idx] = command_b
